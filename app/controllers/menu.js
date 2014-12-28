608eaf070821c825f3ba5853263d17d6b57b0e60
Andres Diaz <andreselduende@gmail.com>
2014-12-28 16:32:52 -0500

    Update styles - Add font icons - Allow hide searchPane in Menu - Hide ActionBar for Android 2.3.x

--- app/controllers/menu.js
+++ app/controllers/menu.js
@@ -2,4 +2,9 @@ exports.setupMenu = function(options) {
     if ( options.title != null ) {
         $.lblText.setText(options.title);
     }
+
+    if ( options.noSearch ) {
+        $.searchPane.hide();
+        $.menu.remove($.searchPane);
+    }
 };
