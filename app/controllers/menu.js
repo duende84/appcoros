a0be49b871c4b39afcf34257ddbdae0721ae7457
Andres Diaz <andreselduende@gmail.com>
2015-01-19 22:07:08 -0500

    Add event click in menu

--- app/controllers/menu.js
+++ app/controllers/menu.js
@@ -1,5 +1,5 @@
 exports.setupMenu = function(options) {
-    if ( options.title != null ) {
+    if ( options.title !== null ) {
         $.lblText.setText(options.title);
     }
 
@@ -7,4 +7,23 @@ exports.setupMenu = function(options) {
         $.searchPane.hide();
         $.menu.remove($.searchPane);
     }
+
+    $.view.addEventListener('click', function(){
+        var dialog = Ti.UI.createAlertDialog({
+            cancel: 2,
+            buttonNames: ['Hymns', 'Coirs','Cancel']
+        });
+
+        dialog.addEventListener('click', function(e){
+            if (e.index === e.source.cancel){
+              Ti.API.info('The cancel button was clicked');
+            }
+
+            Ti.API.info('e.cancel: ' + e.cancel);
+            Ti.API.info('e.source.cancel: ' + e.source.cancel);
+            Ti.API.info('e.index: ' + e.index);
+        });
+
+        dialog.show();
+    });
 };
