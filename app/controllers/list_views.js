9d07b90ddfd95d3cf5091403a5f7b879ba25a2b8
Andres Diaz <andreselduende@gmail.com>
2015-01-19 22:08:58 -0500

    Add list view for hymns and coirs - Add background image to index and list_view windows

--- app/controllers/list_views.js
+++ app/controllers/list_views.js
@@ -2,7 +2,40 @@ var APP      = require('core'),
     params   = arguments[0] || {},
     type     = params.type || '';
 
+var buildList = function(arr){
+    _.map(arr, function(_obj, i) {
+
+    	var item = {
+	        itemName        : { text: _obj.name },
+	        itemType        : { text: _obj.type === 'hymn' ? 'H' : 'C' },
+	        itemButton      : { title: 'Ver' },
+	        itemObject      : _obj,
+	        properties      : {
+	            layout           : 'horizontal',
+	            touchEnabled     : false,
+	            accessoryType    : Ti.UI.LIST_ACCESSORY_TYPE_NONE,
+	            height           : Ti.UI.SIZE,
+	            backgroundColor  : 'transparent'
+	        }
+	    };
+	    if ( OS_IOS ) {
+	        item.properties.selectionStyle = Ti.UI.iPhone.ListViewCellSelectionStyle.NONE;
+	    }
+
+        $.section.appendItems([item]);
+    });
+};
+
 $.menu.setupMenu({ title: type });
 
 APP.addContext($.indexWindow, $.screenWrapper);
 APP.openCurrentWindow();
+
+buildList([
+	{ type: 'coir', name: 'Coro 1' },
+	{ type: 'coir', name: 'Coro 2' },
+	{ type: 'coir', name: 'Coro 3' },
+	{ type: 'hymn', name: 'Himno 1' },
+	{ type: 'hymn', name: 'Himno 2' },
+	{ type: 'hymn', name: 'Himno 3' },
+]);
